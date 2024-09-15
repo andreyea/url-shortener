207fbec80e6c38b409f9a171c11d35b5b031f929
andreyea <andy86e@gmail.com>
2024-09-15 15:46:51 +1000

    Created config

---
+++ internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string     `yaml:"env" env-default:"local" env-required:"true"`
+	StoragePath string     `yaml:"storage_path" env-required:"true"`
+	HttpServer  HttpServer `yaml:"http_server"`
+}
+
+type HttpServer struct {
+	Address     string        `yaml:"address" env-required:"true"`
+	Timeout     time.Duration `yaml:"timeout" env-required:"true"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-required:"true"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// check file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	return &cfg
+}
